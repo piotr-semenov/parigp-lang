2eaab2c3e5725dbebec7ce61c8600f980574f2a3
Piotr Semenov <piotr.k.semenov@gmail.com>
2024-04-13 02:39:21 +0400

    chore: declare a build machine by nix flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,48 @@
 {
-  description = "A very basic flake";
+  description = "Builds TextMate grammar for latest version of PARI/GP";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
   outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default =
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    with import nixpkgs { system = "x86_64-linux"; };
+    let vscode_tmgrammar_test =
+      buildNpmPackage rec {
+        pname = "vscode-tmgrammar-test";
+        version = "0.1.3";
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        src = fetchFromGitHub {
+          owner = "PanAeon";
+          repo = pname;
+          rev = "v${version}";
+          hash = "sha256-p7vHG+VXZOcbhteJ1w/0MbGnt/gY/hk1IuesXUUXUZc=";
+        };
 
+        npmDepsHash = "sha256-MWAQiPt4ajNMDdGaRZ730zsV4KB0AMw4CLbIMAwmFJE=";
+      };
+      newer_pkgs =
+        import (fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/9b570f04bcbb8b2a7f6512708042229519947a94.tar.gz";
+          sha256 = "sha256:1pdx2sc3854narclk8pmxdb1mffv7izhqd7fdfg5brqxr3x00z04";
+        }) {};
+
+      in
+        stdenv.mkDerivation {
+          name = "parigp.tmGrammar";
+          buildInputs = [
+            yq-go
+            gnumake
+            gnused
+            python39
+
+            newer_pkgs.pari
+
+            vscode_tmgrammar_test
+          ];
+          src = self;
+          buildPhase = "make";
+          installPhase = "mkdir -p $out; cp syntaxes/* $out/";
+        };
   };
 }
